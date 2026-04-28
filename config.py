1070269a2db25fbe47a69372060b093540fdafb0
hemanthbreaker2027-cell <hemanthbreaker2027@gmail.com>
2026-04-28 16:59:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -52,8 +52,8 @@ FORCE_PIC = random.choice(ANIME_BANNERS)
 #--------------------------------------------
 SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "arolinks.com")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "e49643875c2fa34dd6087254e58283d65ffc7748")
-RECAPTCHA_SITE_KEY = os.environ.get("RECAPTCHA_SITE_KEY", "6LfvWswsAAAAADgpyWripb1IZSbBjlniKAmdBjSv")
-RECAPTCHA_SECRET_KEY = os.environ.get("RECAPTCHA_SECRET_KEY", "6LfvWswsAAAAAEwc1yAjAVX0IXPsxIe1FHNtwVco")
+RECAPTCHA_SITE_KEY = os.environ.get("RECAPTCHA_SITE_KEY", "6Ldzls4sAAAAAKG_L0GIP_PSFi9_qvWVjxjqKWz0")
+RECAPTCHA_SECRET_KEY = os.environ.get("RECAPTCHA_SECRET_KEY", "6Ldzls4sAAAAAOamczKrn33VyzGV14wbkNpWBUQI")
 SECURE_SECRET_KEY = os.environ.get("SECURE_SECRET_KEY", "HJjdgddjdodkdbdbdmdksksiwkwoahsbdndododjdndndmdkdjdbdmdosjsbsbwkwkwjsbdbdndkdkdkdjdbdbdndndndndna amalapapaksbsbsn")
 WEBSITE_URL = os.environ.get("WEBSITE_URL", "filestore-4-5xip.onrender.com") # e.g. https://yourdomain.com
 JWT_SECRET = os.environ.get("JWT_SECRET", "otakulux_secret_key")
